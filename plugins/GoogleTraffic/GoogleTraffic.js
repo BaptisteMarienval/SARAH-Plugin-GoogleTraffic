c2bc09e15fab4d257a6b85a8b87213e90d8042a1
Baptiste Marienval <baptiste.marienval@gmail.com>
2013-09-03 18:57:13 +0200

    v1

--- plugins/GoogleTraffic/GoogleTraffic.js
+++ plugins/GoogleTraffic/GoogleTraffic.js
@@ -12,26 +12,28 @@
   var begin = config.home;
   
   var url = 'http://maps.google.fr/maps?saddr='+begin+'&daddr='+end;
+ // var url = 'http://www.google.fr/maps/preview?saddr='+begin+'&daddr='+end;
+
   
-    if (data.locomotion=='marche') {
+  if (data.locomotion=='marche') {
   url = url+'&dirflg=w';
   }
     if (data.locomotion=='bike') {
   url = url+'&dirflg=b';
   }
-  
+  console.log(url);
   var request = require('request');
   request({ 'uri' : url }, function (err, response, body){
     
-    if (err || response.statusCode != 200) {
-      callback({'tts': "L'action a échoué"});
-      return;
-    }
+  if (err || response.statusCode != 200) {
+    callback({'tts': "L'action a échoué"});
+    return;
+  }
     
-    var $ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
+  var $ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
    
-   var traffic = $('div.altroute-aux span').text() 
-  //var traffic = $('html.no-maps-mini body.kui div#main.cs div#inner div#page div div#panel.panel-width div#spsizer.cs div div#opanel4.opanel div#panel4.subpanel div#panel_dir.dir div#dir_altroutes.noprint ol#dir_altroutes_body.dir-altroute-mult li#altroute_0.dir-altroute div.dir-altroute-inner div.altroute-aux span').text() ;
+   //var traffic = $('div.altroute-aux span').text() 
+  var traffic = $('html.no-maps-mini body.kui div#main.cs div#inner div#page div div#panel.panel-width div#spsizer.cs div div#opanel4.opanel div#panel4.subpanel div#panel_dir.dir div#dir_altroutes.noprint ol#dir_altroutes_body.dir-altroute-mult li#altroute_0.dir-altroute div.dir-altroute-inner div.altroute-aux span').text() ;
   traffic = traffic.replace('min', 'minutes');
   
   if (traffic) {
@@ -50,6 +52,7 @@
     var traffic = $('html.no-maps-mini body.kui div#main.cs div#inner div#page div div#panel.panel-width div#spsizer.cs div div#opanel4.opanel div#panel4.subpanel div#panel_dir.dir div#dir_altroutes.noprint ol#dir_altroutes_body.dir-altroute-sngl li#altroute_0.dir-altroute div.dir-altroute-inner div.altroute-rcol span').text() ;
 
   }
+  traffic = traffic.replace('km', 'km. ');
   traffic = traffic.replace('min', 'minutes');
   
   }
